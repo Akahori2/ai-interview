1006b00b785e73bb6831315dc9be80146527dbc2
Motoki Akahori <motoki.akahori@gmail.com>
2023-04-15 18:57:00 +0900

    自動フォーマットのためのPrettier導入

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import Head from 'next/head'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,9 +8,9 @@ export default function Home() {
     <>
       <Head>
         <title>AI面接練習</title>
-        <meta name="description" content="AIで面接を練習するアプリです" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name='description' content='AIで面接を練習するアプリです' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
       <p>AI面接練習</p>
     </>
